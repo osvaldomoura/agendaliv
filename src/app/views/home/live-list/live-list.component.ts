52a3a6467b2a8c5168e30f31b5c32e5ef1c0a017
Osvaldo Moura <mouradeveloper@gmail.com>
2021-02-16 00:42:43 -0300

    criar service previou/next

--- src/app/views/home/live-list/live-list.component.ts
+++ src/app/views/home/live-list/live-list.component.ts
@@ -1,3 +1,5 @@
+import { live } from './../../../shared/model/live.model';
+import { LiveService } from './../../../shared/service/live.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,9 +9,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LiveListComponent implements OnInit {
 
-  constructor() { }
+  livesPrevious: live[];
+  livesNext: live[];
+
+  constructor(
+    public LiveService: LiveService
+  ) { }
 
   ngOnInit(): void {
+    this.getLives();
   }
 
+  getLives(){
+    this.LiveService.getLivesWithFlag('previous').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesPrevious);
+      });
+
+    this.LiveService.getLivesWithFlag('next').subscribe(data => {
+      this.livesPrevious = data.content;
+      console.log(this.livesNext);
+      });
+
+  }
 }
